81653a20dd489aa19d4622399da3c5efb0bb9c29
Nomin <nerdene48@gmail.com>
2025-10-18 20:45:58 +1000

    Update: Service provider page content

--- ai-mmi-backup-2025-08-06/resources/views/web/service_provider_info.blade.php
+++ ai-mmi-backup-2025-08-06/resources/views/web/service_provider_info.blade.php
@@ -67,6 +67,90 @@
         </div>
     </div>
 
+    <div class="card-section">
+        <h3 class="card-title"><?php echo $_page_lang['service_provider_info.education_title']; ?></h3>
+        <h4 class="card-subtitle"><?php echo $_page_lang['service_provider_info.education_subtitle']; ?></h4>
+        <div class="iweb-editor">
+            <ul>
+                <li><?php echo $_page_lang['service_provider_info.education_1']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.education_2']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.education_3']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.education_4']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.education_5']; ?></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="card-section">
+        <h3 class="card-title"><?php echo $_page_lang['service_provider_info.agents_title']; ?></h3>
+        <div class="iweb-editor">
+            <ul>
+                <li><?php echo $_page_lang['service_provider_info.agents_1']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.agents_2']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.agents_3']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.agents_4']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.agents_5']; ?></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="card-section">
+        <h3 class="card-title"><?php echo $_page_lang['service_provider_info.migration_title']; ?></h3>
+        <div class="iweb-editor">
+            <ul>
+                <li><?php echo $_page_lang['service_provider_info.migration_1']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.migration_2']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.migration_3']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.migration_4']; ?></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="card-section">
+        <h3 class="card-title"><?php echo $_page_lang['service_provider_info.tutors_title']; ?></h3>
+        <div class="iweb-editor">
+            <ul>
+                <li><?php echo $_page_lang['service_provider_info.tutors_1']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.tutors_2']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.tutors_3']; ?></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="card-section">
+        <h3 class="card-title"><?php echo $_page_lang['service_provider_info.relocation_title']; ?></h3>
+        <div class="iweb-editor">
+            <ul>
+                <li><?php echo $_page_lang['service_provider_info.relocation_1']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.relocation_2']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.relocation_3']; ?></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="card-section">
+        <h3 class="card-title"><?php echo $_page_lang['service_provider_info.accommodation_title']; ?></h3>
+        <h4 class="card-subtitle"><?php echo $_page_lang['service_provider_info.accommodation_subtitle']; ?></h4>
+        <div class="iweb-editor">
+            <ul>
+                <li><?php echo $_page_lang['service_provider_info.accommodation_1']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.accommodation_2']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.accommodation_3']; ?></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="card-section">
+        <h3 class="card-title"><?php echo $_page_lang['service_provider_info.employers_title']; ?></h3>
+        <div class="iweb-editor">
+            <ul>
+                <li><?php echo $_page_lang['service_provider_info.employers_1']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.employers_2']; ?></li>
+                <li><?php echo $_page_lang['service_provider_info.employers_3']; ?></li>
+            </ul>
+        </div>
+    </div>
+
     <h2 class="top-title"><?php echo $_page_lang['service_provider_info.cta_title']; ?></h2>
     <div class="form">
         <p><?php echo $_page_lang['service_provider_info.cta_text']; ?></p>
