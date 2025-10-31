6a054f3b261c1127d25406df2f81a8252677378b
Nomin <nerdene48@gmail.com>
2025-10-31 11:18:09 +1000

    fix: Data deletion info adjusted

--- ai-mmi-backup-2025-08-06/resources/views/web/data_deletion.blade.php
+++ ai-mmi-backup-2025-08-06/resources/views/web/data_deletion.blade.php
@@ -6,7 +6,7 @@
     <div class="clearboth"></div>
 
     <div class="content" style="padding: 20px 0;">
-        <p>If you wish to delete your data, please email us at <a href="mailto:info@ai-mmi-test.com">info@ai-mmi-test.com</a>. We will remove your account and associated data within 48 hours.</p>
+        <p>If you wish to delete your data, please email us at <a href="mailto:info@ai-mmi.com">info@ai-mmi.com</a>. We will remove your account and associated data within 48 hours.</p>
     </div>
 </div>
 @endsection
